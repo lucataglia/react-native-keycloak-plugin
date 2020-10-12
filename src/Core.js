4bc9ad660c4f8fb2c3666f26bfa25436ec3bd956
Alessandro Defendenti <alessandrodefendenti@gmail.com>
2020-10-12 11:56:25 +0200

    removed error from rejection (rejecting json-object with info)

--- src/Core.js
+++ src/Core.js
@@ -86,7 +86,7 @@ const performLogin = async (conf, username, password, scope = 'info') => {
   }
 
   console.error(`Error during kc-api-login, ${fullResponse.status}: ${jsonResponse.error_description}`);
-  return Promise.reject(Error({ ...jsonResponse, status: fullResponse.status }));
+  return Promise.reject({ ...jsonResponse, status: fullResponse.status });
 };
 
 
